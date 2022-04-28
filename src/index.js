12e34bb4e61d1daf1da42e1b081d4752b0a3b2f1
Marian Kuzyk <mariankuzyk@gmail.com>
2022-04-29 01:18:32 +0300

    hz

--- src/index.js
+++ src/index.js
@@ -3,9 +3,20 @@ import debounce from 'lodash.debounce';
 import './css/styles.css';
 
 const countryInfo = document.querySelector('.country-info');
+const countryList = document.querySelector('.country-list');
 const input = document.querySelector('#search-box');
 
 const DEBOUNCE_DELAY = 1000;
+
+function fetchCountries(names) {
+    return fetch(`https://restcountries.com/v3.1/name/${names}`)
+        .then(response => {
+            if (!response.ok) {
+                throw new Error(response.status);
+            }
+            return response.json();
+        })
+};
 
 input.addEventListener('input', debounce(onInput, DEBOUNCE_DELAY));
 
@@ -22,22 +33,12 @@ function onInput(e) {
                 return Notify.info('Too many matches found. Please enter a more specific name.')
             }
 
-            if (country.length > 1 && country.length < 10 ) {
-                return console.log('ok')
+            if (country.length > 1) {
+                return console.log(country);
             }
 
-            if (country.length === 1) {
-                return console.log(country[0])
-            }
+            // return console.log(country[0]);
+            const markup = createCardMarkup(country[0]);
         })
-}
-
-function fetchCountries(name) {
-    return fetch(`https://restcountries.com/v3.1/name/${name}?fields=name,capital,population,languages,flags`)
-        .then(response => {
-            if (!response.ok) {
-                throw new Error(response.status);
-            }
-            return response.json();
-        })
+        .catch(() => { return Notify.failure('Oops, there is no country with that name') });
 };
